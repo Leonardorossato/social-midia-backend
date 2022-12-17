6d99bf712fc2202a04ee47a22a6ce7a72abcfa96
Leonardorossato <leonardorossato21@gmail.com>
2022-12-17 09:47:08 -0300

    assests ok

--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-const helment = require('helment')
-const morgan = require('morgan')
+const path = require("path");
+const helment = require("helment");
+const morgan = require("morgan");
 const mongooseConnection = require("./connection/mongo.connection");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 /*Server configuration*/
 server.use(express.json());
-server.use(helment())
-server.use(helment.crossOriginResourcePolicy({policy: 'cross-origin'}))
-server.use(morgan("common"))
-server.use(cors({origin: 'http://localhost:7000/api', methods: 'CREATE, UPDATE, READ, DELETED'}));
+server.use(helment());
+server.use(helment.crossOriginResourcePolicy({ policy: "cross-origin" }));
+server.use(morgan("common"));
+server.use(
+  cors({
+    origin: "http://localhost:7000/api",
+    methods: "CREATE, UPDATE, READ, DELETED",
+    
+  })
+);
+server.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*Mongo configuration*/
 server.mongooseConnection = mongooseConnection;
