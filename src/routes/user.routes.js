e9e65a8bc058e694210bc662bea55259605a7454
Leonardorossato <leonardorossato21@gmail.com>
2022-12-17 11:32:19 -0300

    tested verfiy token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const UserController = require("../controllers/user.controller");
+const { verifyToken } = require("../middleware/authentication.middleware");
 const router = express.Router();
 
-router.get("/all", UserController.all);
+router.get("/all", verifyToken,UserController.all);
 
 module.exports = router;
