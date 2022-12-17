309cc9ef8d71c286f1807e6fe2379f00ec8bd7c6
Leonardorossato <leonardorossato21@gmail.com>
2022-12-17 09:08:36 -0300

    conenction with mongoDB ok

---
+++ src/connection/mongo.connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose
+.connect(MONGO_URL)
+.then(() => {
+  console.log("Conenction with mongoDB successfully.");
+})
+.catch((error) => {
+  throw new Error("Erro to connect with MongoDB.", error);
+});
+
