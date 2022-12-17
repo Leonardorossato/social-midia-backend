0e49b5192a0f9122230748cd2e3c9484f33a75b1
Leonardorossato <leonardorossato21@gmail.com>
2022-12-17 14:31:20 -0300

    master: Updated master: Added master: Fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,6 +9,56 @@ class UserController {
       throw new Error(error);
     }
   };
+
+  static findOneById = async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json("User with this id not found");
+      }
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).json("User not found");
+    }
+  };
+
+  static deleted = async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json("User with this id not found");
+      }
+      await user.delete(user);
+      return res.status(200).json("User deleted sucessfully");
+    } catch (error) {
+      return res.status(500).json("User not found");
+    }
+  };
+  static getUserFriends = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findById(id);
+
+      const friends = await Promise.all(
+        user.friends.map((id) => User.findById(id))
+      );
+      const formatted = friends.map(
+        ({ firstName, lastName, email, password, picturePath }) => {
+          return {
+            _id,
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+          };
+        }
+      );
+      return res.status(200).json(formatted);
+    } catch (error) {
+      return res.status(500).json("Erro to get all Friends");
+    }
+  };
 }
 
 module.exports = UserController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,8 @@ const UserController = require("../controllers/user.controller");
 const { verifyToken } = require("../middleware/authentication.middleware");
 const router = express.Router();
 
-router.get("/all", verifyToken,UserController.all);
+router.get("/all", verifyToken, UserController.all);
+router.get("/:id", UserController.findOneById);
+router.delete("/:id", UserController.deleted);
 
 module.exports = router;
