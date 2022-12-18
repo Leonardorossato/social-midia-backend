882c6d6c78708a35fa09c04ae7e83d7a27eb38ed
Leonardorossato <leonardorossato21@gmail.com>
2022-12-18 10:27:59 -0300

    clean

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -6,7 +6,7 @@ const postsSchema = new mongoose.Schema(
     firtsName: { type: String, required: true },
     lastName: { type: String, required: true },
     location: { type: String, required: false },
-    description: { String, required: false },
+    description: { String},
     picturePath: { type: String, required: false },
     userPicturePath: { type: String, required: false },
     likes: { type: Map, of: Boolean, required: false },
