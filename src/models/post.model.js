db0233234f6885cd9605dac036df8b6342d54041
Leonardorossato <leonardorossato21@gmail.com>
2022-12-18 09:46:53 -0300

    psot model schema

---
+++ src/models/post.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const postsSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+    name: { type: String, required: true },
+    location: { type: String, required: false },
+    description: { String, required: false },
+    picturePath: { type: String, required: false },
+    userPicturePath: { type: String, required: false },
+    likes: { type: Map, of: Boolean, required: false },
+    comment: { type: Array, default: [] },
+  },
+  { timestamps: new Date() }
+);
+
+const Posts = mongoose.model("Posts", postsSchema);
+module.exports = Posts;
