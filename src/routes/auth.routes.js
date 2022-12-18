466cc586588ad8f029ae95c59d580d7e784fb6ed
Leonardorossato <leonardorossato21@gmail.com>
2022-12-18 09:04:13 -0300

    uplodad a image

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const AuthController = require("../controllers/auth.controller");
 const router = express.Router();
+const upload = multer({ storage });
+const multer = require("multer");
 
 router.post("/login", AuthController.login);
-router.post("/register", AuthController.register);
+router.post("/register", upload.single("picture"), AuthController.register);
 
 module.exports = router;
