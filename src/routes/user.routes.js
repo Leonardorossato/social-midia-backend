297d5989b1c25ad5f243544d96d8c78551fa74cd
Leonardorossato <leonardorossato21@gmail.com>
2022-12-19 13:48:36 -0300

    validate token and obcjet d

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const UserController = require("../controllers/user.controller");
 const { verifyToken } = require("../middleware/authentication.middleware");
+const validObjectId = require("../middleware/validadeId.middleware");
 const router = express.Router();
 
 router.get("/all", verifyToken, UserController.all);
 router.get("/:id", UserController.findOneById);
 router.get("/:id/friends", UserController.getUserFriends);
 router.put("/:id/friendId", UserController.addRemoveFriend);
-router.delete("/:id", UserController.deleted);
+router.delete("/:id",verifyToken, validObjectId ,UserController.deleted);
 
 module.exports = router;
