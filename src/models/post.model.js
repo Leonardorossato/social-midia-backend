5e6c7ff0280c658e1c5c82278b7e5ab7b25ba728
Leonardorossato <leonardorossato21@gmail.com>
2022-12-20 13:28:58 -0300

    fixed and change

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
-const ObjectId = mongoose.Types.ObjectId
+const ObjectId = mongoose.Types.ObjectId;
 
 const postsSchema = new mongoose.Schema(
   {
     userId: { type: ObjectId, ref: "users", required: true },
-    firtsName: { type: String, required: true },
+    name: { type: String, required: true },
     lastName: { type: String, required: true },
     location: { type: String, required: false },
-    description: { String},
+    description: { type: String, required: false },
     picturePath: { type: String, required: false },
     userPicturePath: { type: String, required: false },
     likes: { type: Map, of: Boolean, required: false },
-    comment: { type: Array, default: [] },
+    comment: { type: Array, default: [], required: false },
   },
-  { timestamps: new Date('DD/MM/YY hh:mm:ss') }
+  { timestamps: new Date("DD/MM/YY hh:mm:ss") }
 );
 
 const Posts = mongoose.model("Posts", postsSchema);
