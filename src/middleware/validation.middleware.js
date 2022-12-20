09a81a0943145f04baebf598c7be576afaeab67c
Leonardorossato <leonardorossato21@gmail.com>
2022-12-20 13:48:13 -0300

    ok

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -3,8 +3,7 @@ const passwordComplexity = require("joi-password-complexity");
 
 const validateUser = (user) => {
   const schema = Joi.object({
-    firstName: Joi.string().min(5).max(100).required(),
-    lastName: Joi.string().min(3).max(250).required(),
+    name: Joi.string().min(3).max(250),
     email: Joi.string().email().min(3).max(250).required(),
     password: passwordComplexity(),
     picturePath: Joi.string(),
