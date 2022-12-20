d04a91b63dd92e02274be4bc5fcb6c70ec39e411
Leonardorossato <leonardorossato21@gmail.com>
2022-12-20 13:29:08 -0300

    fixed

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -5,8 +5,12 @@ const validateUser = (user) => {
   const schema = Joi.object({
     firstName: Joi.string().min(5).max(100).required(),
     lastName: Joi.string().min(3).max(250).required(),
-    email: Joi.string().email().min(3).max(200).required(),
+    email: Joi.string().email().min(3).max(250).required(),
     password: passwordComplexity(),
+    picturePath: Joi.string(),
+    friends: Joi.array(),
+    location: Joi.string(),
+    occupation: Joi.string(),
   });
   return schema.validate(user);
 };
@@ -14,8 +18,13 @@ const validateUser = (user) => {
 const validatePosts = (posts) => {
   const schema = Joi.object({
     userId: Joi.string().required(),
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string(),
+    description: Joi.string(),
+    picturePath: Joi.string(),
+    userPicturePath: Joi.string(),
+    comment: Joi.array(),
   });
   return schema.validate(posts);
 };
